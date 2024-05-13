8acfbd4a39d76d1348f330d274ce9e43bbf797d9
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-05-13 19:32:42 -0300

    Excluindo um produto

--- admin.php
+++ admin.php
@@ -57,7 +57,8 @@
             <td><?= $produto->getPrecoFormatado() ?></td>
             <td><a class="botao-editar" href="editar-produto.html">Editar</a></td>
             <td>
-              <form>
+              <form action="excluir-produto.php">
+                  <input type="hidden" name="id" value="<?= $produto->getId() ?>">
                 <input type="submit" class="botao-excluir" value="Excluir">
               </form>
             </td>
