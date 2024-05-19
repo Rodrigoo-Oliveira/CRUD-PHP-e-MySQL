8fc7b709f8ddefdd26d779a0880b94770980118b
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-05-19 20:14:35 -0300

    Ajustes

--- src/conexao-bd.php
+++ src/conexao-bd.php
@@ -1,4 +1,4 @@
 <?php
 
-$pdo = new PDO(dsn:'mysql:host=localhost;dbname=serenatto', username:'root', password: 'R@d248613');
+$pdo = new PDO(dsn:'mysql:host=localhost;dbname=serenatto', username:'root', password: '123456');
 
